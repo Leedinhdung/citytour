a0b6cc43175f8e063ed7d969e1272badbdb3a1ca
Le Dinh Dung <dev.dungld@gmail.com>
2025-08-03 17:04:38 +0700

    feature/fill-data-detail-cruise

--- backend-citytour/src/routes/index.js
+++ backend-citytour/src/routes/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cruiseRoutes from "./admin/cruiseRoutes.js";
 import roomRoutes from "./admin/roomRoutes.js";
+import clientRoutes from "./client/clientRoutes.js";
 
 const router = express.Router();
 
 cruiseRoutes(router);
 roomRoutes(router);
+//Client
+clientRoutes(router);
 export default router;
